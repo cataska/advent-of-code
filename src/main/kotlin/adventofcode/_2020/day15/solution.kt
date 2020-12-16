d1f5e6308f1f29e0528abc31fde644d83642ff99
Wen-Chun Lin <lovecankill@gmail.com>
2020-12-16 09:34:56 +0800

    Optimize
    Less space and faster

--- src/main/kotlin/adventofcode/_2020/day15/solution.kt
+++ src/main/kotlin/adventofcode/_2020/day15/solution.kt
@@ -1,38 +1,22 @@
 package adventofcode._2020.day15
 
-import kotlin.math.absoluteValue
-
 fun main() {
     val input = "9,12,1,4,17,0,18"
     val preinstalled = input.split(",").map(String::toInt)
-    p1(2020, preinstalled).run{ println("P1: $this") }
-    p1(30000000, preinstalled).run{ println("P2: $this") }
+    p(2020, preinstalled).run{ println("P1: $this") }
+    p(30000000, preinstalled).run{ println("P2: $this") }
 }
 
-fun p1(desired: Int, preinstalled: List<Int>): Int {
-    val numbers = preinstalled.toMutableList()
-    val history = mutableMapOf<Int, MutableList<Int>>()
+fun p(desired: Int, preinstalled: List<Int>): Int {
     var last = preinstalled.last()
-
-    numbers.mapIndexed { index, num -> history[num] = mutableListOf(index) }
-    for (i in preinstalled.size until desired) {
-        val next = if (history[last] != null) {
-            if (history[last]!!.size == 1)
-                0
-            else {
-                val occur = history[last]
-                (occur!![occur.size - 1] - occur[occur.size - 2]).absoluteValue
-            }
-        } else last
-
-        if (history[next] == null) {
-            history[next] = mutableListOf(i)
-        } else {
-            history[next]!!.add(i)
-        }
-        numbers.add(next)
+    val history = IntArray(desired) { -1 }
+    for ((i, x) in preinstalled.subList(0, preinstalled.lastIndex).withIndex())
+        history[x] = i + 1
+    var i = preinstalled.size
+    while (i < desired) {
+        val next = if (history[last] == -1) 0 else i - history[last]
+        history[last] = i++
         last = next
     }
-
-    return numbers.last()
+    return last
 }
